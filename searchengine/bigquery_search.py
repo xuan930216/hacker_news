843cafc5c7c44083092aade316ac193fe4ccf25d
Xuan Zhang <xuan@converzion.co>
2018-06-21 21:31:19 +0800

    style fix

--- searchengine/bigquery_search.py
+++ searchengine/bigquery_search.py
@@ -1,10 +1,14 @@
 #import the BigQuery libraries
 from google.cloud import bigquery
 import datetime
+import os.path
+
 
 def query_hackernews(*args):
     #create bigquery client object
-    client = bigquery.Client.from_service_account_json('searchengine/keyfile.json')
+
+    BASE = os.path.dirname(os.path.abspath(__file__))
+    client = bigquery.Client.from_service_account_json(os.path.join(BASE, "keyfile.json"))
     
     #get title, text and date from arguments, then convert title and text to lower case
     title = args[0].lower()
